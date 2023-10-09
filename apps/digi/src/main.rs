c72336a0d557a25811de775b01f3f0ab0a616f4a
Adam Gausmann <adam@gaussian.dev>
2023-10-08 22:30:46 -0500

    Scrolling pixel buffer
    Seems to be performant enough

--- apps/digi/src/main.rs
+++ apps/digi/src/main.rs
@@ -1,16 +1,28 @@
-use slint::{Image, Rgb8Pixel, SharedPixelBuffer};
+use std::{sync::Arc, time::Duration};
+
+use slint::{Image, Rgb8Pixel, SharedPixelBuffer, Timer, TimerMode};
 
 fn main() -> anyhow::Result<()> {
-    let main_window = MainWindow::new()?;
+    let main_window = Arc::new(MainWindow::new()?);
 
-    let mut buffer = SharedPixelBuffer::new(256, 256);
-    let mut pixels =
-        (0..256).flat_map(|r| (0..256).map(move |g| Rgb8Pixel::new(r as u8, g as u8, 0)));
-    buffer
-        .make_mut_slice()
-        .fill_with(move || pixels.next().unwrap());
-    let image = Image::from_rgb8(buffer);
-    main_window.set_map(image);
+    let timer = Timer::default();
+    {
+        let main_window = Arc::clone(&main_window);
+
+        let mut buffer = SharedPixelBuffer::new(256, 256);
+        let mut pixels =
+            (0..256).flat_map(|r| (0..256).map(move |g| Rgb8Pixel::new(r as u8, g as u8, 0)));
+        buffer.make_mut_slice().fill_with(|| pixels.next().unwrap());
+
+        let mut temp = vec![Rgb8Pixel::new(0, 0, 0); 256];
+        timer.start(TimerMode::Repeated, Duration::from_millis(20), move || {
+            temp.copy_from_slice(&buffer.as_slice()[255 * 256..]);
+            buffer.make_mut_slice().copy_within(0..255 * 256, 256);
+            buffer.make_mut_slice()[..256].copy_from_slice(&temp);
+            let image = Image::from_rgb8(buffer.clone());
+            main_window.set_map(image);
+        });
+    }
 
     main_window.run()?;
 
